f09bc60cccbd7abae87c1d805b8621908fc463b2
Carmine <Fierroka@outlook.it>
2021-12-22 19:14:01 +0100

    Aggiunta Metodi nella Servelt Accesso Utente 22/12/21 19:13

--- src/main/java/controller/utente/ServletAccessoUtente.java
+++ src/main/java/controller/utente/ServletAccessoUtente.java
@@ -14,38 +14,40 @@ import java.util.Optional;
 public class ServletAccessoUtente extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       HttpSession session= request.getSession();
-       Utente utente=(Utente) session.getAttribute("utente");
-       String valore=request.getParameter("value");
-        switch (valore){
+      //  HttpSession session = request.getSession();
+    //    Utente utente = (Utente) session.getAttribute("utente");
+        String valore = request.getParameter("value");
+        switch (valore) {
             case "login":
 
-                if (utente==null){
+             /*   if (utente == null) {
 
-                    String email=request.getParameter("emailUser");
-                    String password=request.getParameter("password");
-                    UtenteDAO utenteDAO=new UtenteDAO();
-                    utente=(Utente)utenteDAO.cercaUtentebyEmail(email,password);
+                    String email = request.getParameter("emailUser");
+                    String password = request.getParameter("password");
+                    UtenteDAO utenteDAO = new UtenteDAO();
+                    utente = (Utente) utenteDAO.cercaUtentebyEmail(email, password);
 
-                    if(utente!=null){
-                           if(session.getAttribute("carrello")!=null){
-                              session.removeAttribute("carrello");
-                           }
+                    if (utente != null) {
+                        if (session.getAttribute("carrello") != null) {
+                            session.removeAttribute("carrello");
+                        }
 
-                            session.setAttribute("utente", utente);
-                            response.sendRedirect(response.encodeRedirectURL(request.getContextPath()+"/index.jsp"));
+                        session.setAttribute("utente", utente);
+                        response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/index.jsp"));
 
-                    }else {
-                        request.setAttribute("errore","Utente non trovato");
-                        RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/pagine/formLogin.jsp");
-                        dispatcher.forward(request,response);
+                    } else {
+                        request.setAttribute("errore", "Utente non trovato");
+                        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pagine/formLogin.jsp");
+                        dispatcher.forward(request, response);
                     }
 
-                }
+                }*/
+                loginUtente(request,response);
 
                 break;
             case "logout":
-                if(utente!=null){
+                logoutUtente(request,response);
+               /* if(utente!=null){
 
                     session.removeAttribute("utente");
                     Carrello carrello=(Carrello) session.getAttribute("carrello");
@@ -54,13 +56,57 @@ public class ServletAccessoUtente extends HttpServlet {
                     }
 
                     response.sendRedirect(response.encodeRedirectURL(request.getContextPath()+"/index.jsp"));
-                }
+                }*/
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-       doGet(request,response);
+        doGet(request, response);
+    }
+
+    private void logoutUtente(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        Utente utente = (Utente) session.getAttribute("utente");
+        if (utente != null) {
+
+            session.removeAttribute("utente");
+            Carrello carrello = (Carrello) session.getAttribute("carrello");
+            if (carrello != null) {
+                session.removeAttribute("carrello");
+            }
+
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/index.jsp"));
+        }
+
+    }
+
+    private void loginUtente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Utente utente = (Utente) session.getAttribute("utente");
+
+        if (utente == null) {
+
+            String email = request.getParameter("emailUser");
+            String password = request.getParameter("password");
+            UtenteDAO utenteDAO = new UtenteDAO();
+            utente = (Utente) utenteDAO.cercaUtentebyEmail(email, password);
+
+            if (utente != null) {
+                if (session.getAttribute("carrello") != null) {
+                    session.removeAttribute("carrello");
+                }
+
+                session.setAttribute("utente", utente);
+                response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/index.jsp"));
+
+            } else {
+                request.setAttribute("errore", "Utente non trovato");
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pagine/formLogin.jsp");
+                dispatcher.forward(request, response);
+            }
+
+        }
     }
 }
