e0613b8b9edc884fbaf699abdbf6c47538144e80
Carmine <Fierroka@outlook.it>
2022-01-13 17:02:34 +0100

    servletAggiungiAlCarrello testing

--- src/main/java/controller/carrello/ServletAggiungiAlCarrello.java
+++ src/main/java/controller/carrello/ServletAggiungiAlCarrello.java
@@ -8,6 +8,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ServletAggiungiAlCarrello", value = "/ServletAggiungiAlCarrello")
 public class ServletAggiungiAlCarrello extends HttpServlet {
@@ -16,11 +17,13 @@ public class ServletAggiungiAlCarrello extends HttpServlet {
     private Carrello carrello;
     private Utente utente;
     private Prodotto prodotto;
+    private int totale;
 
     public ServletAggiungiAlCarrello(){
         prodottoDAO=new ProdottoDAO();
         utente= new Utente();
         prodotto= new Prodotto();
+        carrello= new Carrello();
     }
 
     public ServletAggiungiAlCarrello(ProdottoDAO prodottoDAO,Carrello carrello,Utente utente,Prodotto prodotto){
@@ -33,8 +36,18 @@ public class ServletAggiungiAlCarrello extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int idProdotto=Integer.parseInt(request.getParameter("prodotto"));
-        aggiuntaAlCarrello(idProdotto, request, response);
-        response.getWriter().write("Prodotto aggiunto al carrello!");
+         totale=1;
+        if(request.getParameter("totale")!=null){
+            totale=Integer.parseInt(request.getParameter("totale"));
+        }
+        HttpSession session=request.getSession();
+        carrello=(Carrello) session.getAttribute("carrello");
+        utente=(Utente) session.getAttribute("utente");
+        prodotto= prodottoDAO.cercaProdotto(idProdotto);
+        aggiuntaAlCarrello(idProdotto,session);
+        PrintWriter printWriter= response.getWriter();
+        printWriter.write("Prodotto Aggiunto al Carrello");
+//         response.getWriter().write("Prodotto aggiunto al carrello!");
     }
 
     @Override
@@ -51,28 +64,20 @@ public class ServletAggiungiAlCarrello extends HttpServlet {
      * @throws IOException
      * @post getNumProdotti = @pre getNumProdotti + 1
      */
-    public void aggiuntaAlCarrello(int idProdotto, HttpServletRequest request, HttpServletResponse response) throws IOException{
-        int totale=1;
-
-        if(request.getParameter("totale")!=null){
-            totale=Integer.parseInt(request.getParameter("totale"));
-        }
-        //int prezzoTotale=0;
-        HttpSession session=request.getSession();
+    public void aggiuntaAlCarrello(int idProdotto,HttpSession session) throws IOException{
+        /*HttpSession session=request.getSession();
          carrello=(Carrello) session.getAttribute("carrello");
-
          utente=(Utente) session.getAttribute("utente");
-
-         prodotto= prodottoDAO.cercaProdotto(idProdotto);
+         prodotto= prodottoDAO.cercaProdotto(idProdotto);*/
         if(utente!=null){
             if(utente.getCarrello()!=null){
                 prodotto.setPrezzoQuantita(totale);
                 utente.getCarrello().addProdotto(prodotto);
             }else {
-                Carrello carrello1= new Carrello();
+                carrello= new Carrello();
                 prodotto.setPrezzoQuantita(totale);
-                carrello1.addProdotto(prodotto);
-                utente.setCarrello(carrello1);
+                carrello.addProdotto(prodotto);
+                utente.setCarrello(carrello);
             }
         }else{
             if(carrello!=null) {
